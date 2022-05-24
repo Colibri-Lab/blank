26281cc05ff972a0d506275c90e1fb9d422d061a
Vahan Grigoryan <vahan.grigoryan@gmail.com>
2022-05-25 00:46:53 +0300

    -

--- templates/index.php
+++ templates/index.php
@@ -3,6 +3,8 @@
 use Colibri\App;
 use Colibri\Utils\Cache\Bundle;
 use Colibri\Web\Templates\PhpTemplate;
+use App\Modules\Sites\Models\Domain;
+use App\Modules\Sites\Models\Domains;
 
 $langModule = App::$moduleManager->lang;
 
@@ -18,12 +20,14 @@ $langModule = App::$moduleManager->lang;
         <link rel="stylesheet" href="<?=Bundle::Automate(App::$domainKey.($langModule ? '.'.$langModule->current : '').'.assets.css', 'scss', array_merge(
             [['path' => App::$appRoot.'vendor/colibri/ui/src/']], 
             [['path' => App::$webRoot.'res/css/']], 
-            App::$moduleManager->GetPaths('.Bundle/')
+            App::$moduleManager->GetPaths('.Bundle/'),
+            App::$moduleManager->GetPaths('templates/')
         ))?>" type="text/css">
 
         <script type="text/javascript" src="<?=Bundle::Automate(App::$domainKey.($langModule ? '.'.$langModule->current : '').'.assets.js', 'js', array_merge(
             [['path' => App::$appRoot.'vendor/colibri/ui/src/', 'exts' => ['js', 'html']]], 
             App::$moduleManager->GetPaths('.Bundle/', ['exts' => ['js', 'html']]), 
+            App::$moduleManager->GetPaths('templates/', ['exts' => ['js', 'html']]), 
         ))?>"></script>
 
 
