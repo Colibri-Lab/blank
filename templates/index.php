214ee43dc0f942e3fb473057c3439d582d82f2bc
Vahan Grigoryan <vahan.grigoryan@gmail.com>
2022-05-26 00:47:16 +0300

    -

--- templates/index.php
+++ templates/index.php
@@ -14,22 +14,7 @@ $langModule = App::$moduleManager->lang;
     <head>
 
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    
         <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE9" />
-
-        <link rel="stylesheet" href="<?=Bundle::Automate(App::$domainKey.($langModule ? '.'.$langModule->current : '').'.assets.css', 'scss', array_merge(
-            [['path' => App::$appRoot.'vendor/colibri/ui/src/']], 
-            [['path' => App::$webRoot.'res/css/']], 
-            App::$moduleManager->GetPaths('.Bundle/'),
-            App::$moduleManager->GetPaths('templates/')
-        ))?>" type="text/css" />
-
-        <script type="text/javascript" src="<?=Bundle::Automate(App::$domainKey.($langModule ? '.'.$langModule->current : '').'.assets.js', 'js', array_merge(
-            [['path' => App::$appRoot.'vendor/colibri/ui/src/', 'exts' => ['js', 'html']]], 
-            App::$moduleManager->GetPaths('.Bundle/', ['exts' => ['js', 'html']]), 
-            App::$moduleManager->GetPaths('templates/', ['exts' => ['js', 'html']]), 
-        ))?>"></script>
-
 
         <?php
             $headerTemplates = App::$moduleManager->GetTemplates('header');
@@ -49,6 +34,20 @@ $langModule = App::$moduleManager->lang;
             }
         ?>
     </body>
+    
+    <link rel="stylesheet" href="<?=Bundle::Automate(App::$domainKey.($langModule ? '.'.$langModule->current : '').'.assets.css', 'scss', array_merge(
+        [['path' => App::$appRoot.'vendor/colibri/ui/src/']], 
+        [['path' => App::$webRoot.'res/css/']], 
+        App::$moduleManager->GetPaths('.Bundle/'),
+        App::$moduleManager->GetPaths('templates/')
+    ))?>" type="text/css" />
+
+    <script type="text/javascript" src="<?=Bundle::Automate(App::$domainKey.($langModule ? '.'.$langModule->current : '').'.assets.js', 'js', array_merge(
+        [['path' => App::$appRoot.'vendor/colibri/ui/src/', 'exts' => ['js', 'html']]], 
+        App::$moduleManager->GetPaths('.Bundle/', ['exts' => ['js', 'html']]), 
+        App::$moduleManager->GetPaths('templates/', ['exts' => ['js', 'html']]), 
+    ))?>"></script>
+
     <?php
         $footerTemplates = App::$moduleManager->GetTemplates('footer');
         foreach($footerTemplates as $template) {
