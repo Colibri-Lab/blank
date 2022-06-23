94a741221374a32b657406f6e7d0d61c9e5a304e
Vahan Grigoryan <vahan.grigoryan@gmail.com>
2022-06-23 07:18:43 +0000

    -

--- templates/index.php
+++ templates/index.php
@@ -32,7 +32,7 @@ foreach($footerTemplates as $template) {
 
 ?>
 <!DOCTYPE html>
-<html lang="<?=$langModule->current?>">
+<html lang="<?=($langModule ? $langModule->current : '')?>">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE9" />
