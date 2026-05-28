9e8583a00c6335e353ca9802e25c4803f84f4e55
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2026-05-28 08:40:32 +0400

    -

--- web/server.php
+++ web/server.php
@@ -7,7 +7,13 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Colibri\ReactServer;
 use Colibri\Utils\Config\Config;
 
-$config = Config::LoadFile('app.yaml');
-$configArray = $config->Query('reactserver')->AsArray();
+$config = \yaml_parse_file(__DIR__ . '/../config/app.yaml');
+$reactserver = $config['reactserver'];
+if(strstr($reactserver, 'include(') !== false) {
+    $reactserver = str_replace(['include(', ')'], '', $reactserver);
+    $reactserver = \yaml_parse_file(__DIR__ . '/../config/' . $reactserver);
+    $config['reactserver'] = $reactserver;
+}
+$configArray = $config['reactserver'];
 
-ReactServer::Initialize($configArray);
+ReactServer::Initialize($configArray, __DIR__);
